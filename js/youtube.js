0981fdf916ccf9b656be41782fc20e88522522ad
nurul auni afifah <nurulauni.1453@gmail.com>
2024-07-09 15:13:24 +0700

    commit 2

--- js/youtube.js
+++ js/youtube.js
@@ -6,19 +6,16 @@ fetch('https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxR
     console.log(data);
     data.items.forEach(al => {
         youtubePlaylist.innerHTML += `
-        <div class="col-md-3">
-    <div class="card">
-      <iframe class="card-img-top" src="https://www.youtube.com/embed/${al.snippet.resourceId.videoid}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-      <div class="card-body">
-        <p>${al.snippet.title}</p>
-        <small>${al.snippet.publishedAt}</small>
-      </div>
-    </div>
-  </div>
+        <div class="col-md-3 mb-4">
+            <div class="card">
+            <iframe class="card-img-top"src="https://www.youtube.com/embed/${al.snippet.resourceId.videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <div class="card-body">
+                <p>${al.snippet.title}</p>
+                <small>${al.snippet.publishedAt}</small>
+            </div>
+            </div>
+        </div>
         `
-        
-        
-
     })
 
 })
